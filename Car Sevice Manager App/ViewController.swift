07799f8ec7bf6f677a2bb34d2f33c3b153ed4bc8
Murat Baykor <foytingo@gmail>
2020-08-17 13:48:10 +0300

    Initial Commit

---
+++ Car Sevice Manager App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Car Sevice Manager App
+//
+//  Created by Murat Baykor on 17.08.2020.
+//  Copyright © 2020 Murat Baykor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Car Sevice Manager AppTests/Car_Sevice_Manager_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Car_Sevice_Manager_AppTests.swift
+//  Car Sevice Manager AppTests
+//
+//  Created by Murat Baykor on 17.08.2020.
+//  Copyright © 2020 Murat Baykor. All rights reserved.
+//
+
+import XCTest
+@testable import Car_Sevice_Manager_App
+
+class Car_Sevice_Manager_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
